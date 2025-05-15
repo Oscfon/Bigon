56fcb084f84a35f14404b0323d3bf84bdfe79d98
Oscfon <165289370+Oscfon@users.noreply.github.com>
2025-05-15 09:47:43 +0200

    Area computation

--- bigon_detection.py
+++ bigon_detection.py
@@ -92,6 +92,10 @@ def tree_contraction(G, treecotree):
         - None if e is an edge of the tree of treecotree
         - d such that fp[d]=e in G after contracting the edge of the tree if e is an edge of the cotree
         - the corresponding edge of F otherwise
+    Compute a list rank such that rank[e] is:
+        - None if e is an edge of the tree of treecotree
+        - the index of e around the vertex if e is an edge of the cotree
+        - the total number of edge of the cotree between e and the next edge in F
 
     INPUT:
 
@@ -102,34 +106,47 @@ def tree_contraction(G, treecotree):
     """
     fp=G.face_permutation(copy=False)
     correspondence = [None for _ in G.darts()]
+    rank = [None for _ in G.darts()]
     nfp=[None for _ in range(4*G.genus())]
+    recor = [None for _ in range(4*G.genus())]
     i=0
     while treecotree[i]!=2:
         i+=2
     last=0
     correspondence[i]=0
     correspondence[i+1]=1
+    recor[0]=i
+    recor[1]=i+1
     e=fp[i]
     free_index=2
+    r=0
     
     while e!=i:
         e1=e+1 if e%2==0 else e-1
         if treecotree[e]==1:
             correspondence[e]=last
+            rank[e]=r
+            r+=1
             e=fp[e1]
+            
         elif treecotree[e]==0:
             e=fp[e]
         else:
             if correspondence[e]==None:
                 correspondence[e]=free_index
                 correspondence[e1]=free_index+1
+                recor[free_index]=e
+                recor[free_index+1]=e1
                 free_index+=2
+            rank[e]=r
+            r=0
             nfp[last]=correspondence[e]
             last=correspondence[e]
             e=fp[e]
+    rank[i]=r
     nfp[last]=0
     F=FatGraph(fp=nfp)
-    return F,correspondence
+    return F, correspondence, recor, rank
             
 def quad_system(G, treecotree):
     r"""
@@ -150,7 +167,7 @@ def quad_system(G, treecotree):
     - cor2 a list such that cor2[e] for a e a dart of G is the list of darts of Q corresponding to e
     """
     
-    (F,cor)=tree_contraction(G,treecotree)
+    (F,cor, _, _)=tree_contraction(G,treecotree)
     vp=F.vertex_permutation(copy=False)
     fp=F.face_permutation(copy=False)
     qvp=[None for _ in range(8*G.genus())]
@@ -211,11 +228,14 @@ def turn(Q,e,f):
     return turn
 
 def turn_add(t,turn,num):
+    if num==0:
+        return
     if len(t)==0:
         t.append((turn,num))
     else:
         if t[-1][0]==turn:
-            t[-1][1]+=num
+            (a,b)=t.pop()
+            t.append((a,b+1))
         else:
             t.append((turn,num))
 
@@ -235,7 +255,6 @@ def simplification(Q,d,c,t,e):
     r"""
     Update the geodesic representant c and the turn sequence t after adding the edge e to the path.
 
-
     INPUT:
 
     - Q -- underlying quad system 
@@ -246,21 +265,22 @@ def simplification(Q,d,c,t,e):
 
     OUTPUT
 
-    ``None``. This function update the value of c and t.
+    Return a boolean saying if we flatten a bracket or did a spur removal.
+    This function update the value of c and t.
     """
     fp=Q.face_permutation(copy=False)
     if len(c)==0:
         c.append(e)
-        return
+        return False
     elif len(c)==1:
         newturn = turn(Q,c[-1],e)
         if newturn==0:
             c.pop()
-            return
+            return True
         else:
             t.append((newturn,1))
             c.append(e)
-            return
+            return False
     last=c[-1]
     (last_t, n_last)=t.pop()
     newturn=turn(Q,last,e)
@@ -268,59 +288,167 @@ def simplification(Q,d,c,t,e):
         c.pop()
         if n_last!=1:
             t.append((last_t,n_last-1))
+        return True
     elif newturn==1 and last_t==1:
-        c.pop()
-        edge=c.pop()
-        edge1=edge+1 if edge%2==0 else edge-1
-        c.pop()
-        c.append(fp[fp[edge1]])
-        if n_last!=1:
-            raise ValueError("The path wasn't geodesic")
-        turn_modif(t,-1,d)
+        bracket_removal(c, t, True, 0, d)
     elif newturn==d-1 and last_t==d-1:
-        c.pop()
-        edge=fp[fp[c.pop()]]
-        edge1=edge+1 if edge%2==0 else edge-1
-        c.pop()
-        c.append(edge1)
-        if n_last!=1:
-            raise ValueError("The path wasn't geodesic")
-        turn_modif(t,1,d)
+        bracket_removal(c, t, False, 0, d)
+        return True
     elif len(t)>=2 and newturn==1 and last_t==2 and t[-1][0]==1:
-        c.pop()
-        l=[]
-        for _ in range(n_last+1):
-            edge=c.pop()
-            edge1=edge+1 if edge%2==0 else edge-1
-            l.append(fp[fp[edge1]])
-        c.pop()
-        c=c+l
-        (t2,n2)=t.pop()
-        if n2!=1:
-            raise ValueError("The path wasn't geodesic")
-        turn_modif(t,-1,d)
-        turn_add(t,d-2,n_last)
+        bracket_removal(c, t, True, n_last, d)
+        return True
     elif len(t)>=2 and newturn==d-1 and last_t==d-2 and t[-1][0]==d-1:
-        c.pop()
-        l=[]
-        for _ in range(n_last+1):
-            edge=fp[fp[c.pop()]]
-            edge1=edge+1 if edge%2==0 else edge-1
-            l.append(edge1)
-        c.pop()
-        c=c+l
-        (t2,n2)=t.pop()
-        if n2!=1:
-            raise ValueError("The path wasn't geodesic")
-        turn_modif(t,1,d)
-        turn_add(t,2,n_last)
+        bracket_removal(c, t, False, n_last, d)
+        return True
     elif newturn==last_t:
         c.append(e)
         t.append(newturn,n_last+1)
+        return False
     else:
         c.append(e)
         t.append((last_t,n_last))
         t.append((newturn,1))
+        return False
+
+
+def spur_removal(c,t):
+    c.pop()
+    c=c[1:]
+    (t1,n1)=t.pop()
+    if n1>1:
+        t.append(t1,n1-1)
+    (t2,n2)=t[0]
+    if n2>1:
+        t[0]=(t2,n2)
+    else:
+        t=t[1:]
+
+def bracket_removal(c, t, positive, length, d, end=True):
+    if end and positive:
+        l=[]
+        for i in range(length+1,0,-1):
+            edge=c[-i]
+            edge1=edge+1 if edge%2==0 else edge-1
+            l.append(fp[fp[edge1]])
+        c=c[:-length-2]
+        c=c+l
+        (t2,n2)=t.pop()
+        if n2!=1:
+            raise ValueError("The path wasn't geodesic")
+        turn_modif(t,-1,d)
+        turn_add(t,d-2,length)
+    elif end:
+        l=[]
+        for i in range(length+1,0,-1):
+            edge=fp[fp[c[-i]]]
+            edge1=edge+1 if edge%2==0 else edge-1
+            l.append(edge1)
+        c=c[:-length-2]
+        c=c+l
+        (t2,n2)=t.pop()
+        if n2!=1:
+            raise ValueError("The path wasn't geodesic")
+        turn_modif(t,1,d)
+        turn_add(t,2,length)
+    elif positive:
+        l=[]
+        for i in range(length+1):
+            edge=c[i]
+            edge1=edge+1 if edge%2==0 else edge-1
+            l.append(fp[fp[edge1]])
+        c=c[length+2:]
+        c=l+c
+    else:
+        l=[]
+        for i in range(length+1):
+            edge=fp[fp[c[-i]]]
+            edge1=edge+1 if edge%2==0 else edge-1
+            l.append(edge1)
+        c=c[length+2:]
+        c=l+c
+
+def origin_simplification(Q, d, c, t):
+    r"""
+    Perform the geodesic simplification at the based point of c
+
+    """
+    first_turn = turn(Q, c[-1], c[0])
+    if first_turn == 0:
+        spur_removal(c,t)
+        origin_simplification(Q, d, c, t)
+    elif first_turn==1 and len(t)==1 and t[0][0]==2:
+        c.pop()
+        c=c[1:]
+        l=[]
+        for e in c:
+            e1=e+1 if e%2==0 else e-1
+            l.append(fp[fp[e1]])
+        c=l
+        t=[(d-2, t[0][1]-2)]
+    elif first_turn==d-1 and len(t)==1 and t[0][0]==d-2:
+        c.pop()
+        c=c[1:]
+        l=[]
+        for e in c:
+            e1=e+1 if e%2==0 else e-1
+            l.append(fp[fp[e1]])
+        c=l
+        t=[(2, t[0][1]-2)]
+    elif first_turn==2 and len(t)>=3:
+
+
+
+
+
+        
+    elif simplification(Q,d,c,t,c[0]):
+        c=c[1:]
+        first_turn = turn(Q, c[-1], c[0])
+        while first_turn==0:
+            spur_removal(c,t)
+            first_turn = turn(Q, c[-1], c[0])
+    else:
+            
+            
+
+
+
+
+
+
+    
+    elif first_turn==1 and len(t)>=1 and t[0][0]==1:
+        c.pop()
+        e=c[0]
+        e1=e+1 if e%2==0 else e-1
+        c=c[1:]
+        c[0]=fp[fp[e1]]
+        if t[0][1]!=1:
+            raise ValueError("The path wasn't geodesic")
+        elif t[1][1]==1:
+            t=t[1:]
+            t[0]=(t[0][0]-1,1)
+        else:
+            t[1]=(t[1][0],t[1][1]-1)
+            t[0]=(t[1][0]-1,1)
+        origin_simplification(Q, d, c, t)
+    elif first_turn==d-1 and len(t)>=1 and t[0][0]==d-1:
+        c.pop()
+        e=fp[fp[c[0]]]
+        e1=e+1 if e%2==0 else e-1
+        c=c[1:]
+        c[0]=e1
+        if t[0][1]!=1:
+            raise ValueError("The path wasn't geodesic")
+        elif t[1][1]==1:
+            t=t[1:]
+            t[0]=(t[0][0]+1,1)
+        else:
+            t[1]=(t[1][0],t[1][1]-1)
+            t[0]=(t[1][0]+1,1)
+        origin_simplification(Q, d, c, t)
+    elif first_turn==1 and len(t)>=2 and 
+    
 
 
 def representant(w, Q, cor):
@@ -360,5 +488,97 @@ def is_homotopic(G,w1,w2,Q=None, cor=None):
     c1, t1=representant(w1, Q, cor)
     c2, t2=representant(w2, Q, cor)
     return c1==c2
+
+
+def forms(D):
+    r"""
+    Compute two one forms on the edges of D such that omega wedge eta = 2
+    """
+    omega = [0 for _ in D.darts()]
+    eta = [0 for _ in D.darts()]
+    omega[0]=1
+    omega[1]=-1
+    fp=D.face_permutation(copy=False)
+    found1=False
+    e=0
+    goodedge= [0 for _ in D.darts()]
+    for _ in D.darts():
+        e=fp[e]
+        e1=e+1 if e%2==0 else e-1
+        if e==1:
+            found1=True
+        elif found1:
+            goodedge[e]-=1
+            goodedge[e1]+=1
+        else:
+            goodedge[e]+=1
+            goodedge[e1]-=1
+    e=0
+    while goodedge[e]!=2:
+        e+=1
+    eta[e]=1
+    e1=e+1 if e%2==0 else e-1
+    eta[e1]=-1
+    return omega,eta
+
+
+def area_precomuputation(G, treecotree):
+    r"""
+    Precompute four lists that store the information to compute the area of a contractible path.
+    """
+    
+    F, cor, recor, rank = tree_contraction(G, treecotree)
+    fpF=F.face_permutation(copy=False)
+    omegaF, etaF = forms(F)
+    local_area=[0 for _ in G.darts()]
+    omega=[0 for _ in G.darts()]
+    eta=[0 for _ in G.darts()]
+    integral=[0 for _ in G.darts()]
+    for e in G.darts():
+        if treecotree[e]==2:
+            omega[e]=omegaF[cor[e]]
+            eta[e]=etaF[cor[e]]
+            integral[e]=omegaF[cor[e]]*etaF[cor[e]]/2
+        elif treecotree[e]==1:
+            start=cor[e]
+            e1=e+1 if e%2==0 else e-1
+            end=cor[e1]
+            current = start
+            om=0
+            et=0
+            it=0
+            alpha=0
+            la=rank[e1]-rank[e]-1
+            while current != end:
+                current=fpF[current]
+                om+=omegaF[current]
+                et+=etaF[current]
+                it+= (alpha+omegaF[current]/2)*etaF[current]
+                alpha+=omegaF[current]
+                la+=rank[recor[current]]
+            omega[e]=om
+            eta[e]=et
+            integral[e]=it
+            local_area[e]=la//2+1
+    return local_area, omega, eta, integral
+                
+def area(G, w, local_area, omega, eta, integral):
+    r"""
+    Compute the area of a contractible path
+    """
+    dom=0
+    area=0
+    alpha=0
+    for e in w:
+        dom += integral[e]+ eta[e]*alpha
+        area += local_area[e]
+        alpha += omega[e]
+    return dom*G.num_faces()-area
+
+
+
+
+
+
 
 
